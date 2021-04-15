c5090577f5f2480db8d33b7741269a24b9b553cb
Sariel09 <82573854+Sariel09@users.noreply.github.com>
2021-04-15 16:35:37 +0800

    Add files via upload

---
+++ sariel/src/main/java/com/demo/sariel/SarielApplication.java
@@ -0,0 +1,13 @@
+package com.demo.sariel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarielApplication.class, args);
+	}
+
+}
---
+++ sariel/src/main/java/com/demo/sariel/config/LoginHandlerInterceprtor.java
@@ -0,0 +1,23 @@
+package com.demo.sariel.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginHandlerInterceprtor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if (loginUser == null) {
+            request.setAttribute("msg", "没有权限");
+            request.getRequestDispatcher("/index.html").forward(request, response);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
